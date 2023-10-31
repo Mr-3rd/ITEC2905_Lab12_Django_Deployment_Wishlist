f6be6dd1a60d4bf47e0efe1af484490079acb88e
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-10-31 17:29:28 -0500

    creating the first video files

---
+++ wishlist/travel_wishlist/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=200)
+    visited = models.BooleanField(default=False)
+
+    def __str__(self):
+        # Useful information for the developer
+        return f'{self.name} visited? {self.visited}'
+
+
+    
+    
+
