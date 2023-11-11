05445414ef2d03a9e490a9355a3955797fb4ca00
Mr. 3rd <97487150+Mr-3rd@users.noreply.github.com>
2023-11-11 14:28:28 -0600

    Completed unit tests - moving to selenium

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -58,3 +58,54 @@ class TestVisitedList(TestCase):
         self.assertNotContains(response, 'New York')
         self.assertContains(response, 'San Francisco')
         self.assertContains(response, 'Moab')
+
+
+class TestAddNew(TestCase):
+
+    def test_add_new_not_visited(self):
+        add_place_url = reverse('place_list')
+        new_place_data = {'name': 'Tokyo', 'visited' : False}
+
+        # create a response that will follow the redirect when adding a new place
+        response = self.client.post(add_place_url, new_place_data, follow=True)
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        # get the information for the relevant places from the site view
+        response_places = response.context['places']
+        #ensure that only one place is added
+        self.assertEqual(1, len(response_places))  
+        tokyo_in_resp = response_places[0]
+
+        tokyo_in_db = Place.objects.get(name='Tokyo', visited=False)
+
+        self.assertEqual(tokyo_in_db, tokyo_in_resp)
+
+
+class TestVisitedPlace(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_visiting_place(self):
+        # utilize reverse to fetch the dynamic url of the place being updated to visited
+        visit_place_url = reverse('place_visited', args=(2, )) # 2 is the location for new york
+
+        response = self.client.post(visit_place_url, follow=True)
+
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+
+        self.assertContains(response, 'New York')
+
+        self.assertNotContains(response, 'Tokyo')
+
+        new_york = Place.objects.get(pk=2)
+        self.assertTrue(new_york.visited)
+
+    def test_not_exist_place(self):
+
+        visit_no_place_url = reverse('place_visited', args=(999999999999999, ))
+        response = self.client.post(visit_no_place_url, follow=True)
+        self.assertEqual(404, response.status_code)
+
+
+
+
+
